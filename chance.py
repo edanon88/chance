79d813c1d5296bc9f14641aa56ce1f0472d8410a
EAFD88 <edd.danon@gmail.com>
2020-10-06 14:51:19 +0100

    Initial commit

---
+++ chance.py
@@ -0,0 +1,14 @@
+# Codecademy Games of Chance
+
+import random
+
+money = 100
+
+#Write your game of chance functions here
+print(random.randint(1,10))
+
+
+
+
+
+#Call your game of chance functions here
